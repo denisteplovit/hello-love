e3a94e6656faf0feb72bb7f025f8e85c4793f53b
denisteplovit <super.den20152015@yandex.ru>
2024-09-05 10:59:12 +0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const loveword = document.querySelector('.loveword');
+const interesting = document.querySelector('.interesting');
+const outputloveword = document.querySelector('.outputloveword');
+const outputinteresting = document.querySelector('.outputinteresting');
+
+loveword.addEventListener("click", function () {
+    outputloveword.innerHTML = "Любименькая моя, я тебя безумненько сильненько люблю!!! Ты самая лучшенькая!!! Мы пройдём все трудности вместе, со всем на свете справимся!!!"
+});
+
+interesting.addEventListener('click', function () {
+    outputinteresting.innerHTML = "*невненькоу поцеловал любименькую малыфку в шладенькие губоньки"
+});
